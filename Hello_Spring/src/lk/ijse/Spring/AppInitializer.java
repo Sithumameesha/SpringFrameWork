ec2d3f1b4c633a340bc0178d835d298e73053f36
Sithum <sithumameesha76@gmail.com>
2023-01-06 11:35:45 +0530

    Method Involked

--- Hello_Spring/src/lk/ijse/Spring/AppInitializer.java
+++ Hello_Spring/src/lk/ijse/Spring/AppInitializer.java
@@ -1,6 +1,7 @@
 package lk.ijse.Spring;
 
 import lk.ijse.Spring.config.AppConfige;
+import lk.ijse.Spring.pojo.pojoTwo;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AppInitializer {
@@ -9,6 +10,12 @@ public class AppInitializer {
         AnnotationConfigApplicationContext ctx= new AnnotationConfigApplicationContext();
         ctx.register(AppConfige.class);
         ctx.refresh();
+
+        //Context Invocation
+        pojoTwo c1 = ctx.getBean(pojoTwo.class);
+        System.out.println(c1);
+        c1.Name();
+
         ctx.close();
     }
 
--- Hello_Spring/src/lk/ijse/Spring/pojo/pojoTwo.java
+++ Hello_Spring/src/lk/ijse/Spring/pojo/pojoTwo.java
@@ -8,4 +8,7 @@ public class pojoTwo {
     public pojoTwo(){
         System.out.println("Hello PojoTwo");
     }
+    public void Name(){
+        System.out.println("My Name Is Sithum");
+    }
 }
