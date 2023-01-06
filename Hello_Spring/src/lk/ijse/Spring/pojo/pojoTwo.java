fe8a93598ab52b64120705d59bc6ddcf8fd5b973
Sithum <sithumameesha76@gmail.com>
2023-01-06 11:16:14 +0530

    Di Mechanisum

---
+++ Hello_Spring/src/lk/ijse/Spring/pojo/pojoTwo.java
@@ -0,0 +1,11 @@
+package lk.ijse.Spring.pojo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class pojoTwo {
+
+    public pojoTwo(){
+        System.out.println("Hello PojoTwo");
+    }
+}
