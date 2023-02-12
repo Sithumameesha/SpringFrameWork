5dffa5d77056841c53fb2eb1e08c511a275c739e
Sithum <sithumameesha76@gmail.com>
2023-02-13 00:54:43 +0530

    Content Updated with new Dto

--- Spring_09/src/lk/ijse/spring/controller/CustomerController.java
+++ Spring_09/src/lk/ijse/spring/controller/CustomerController.java
@@ -1,5 +1,6 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.Dto.CustomerDto;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -7,8 +8,8 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin
 public class CustomerController {
     @PostMapping
-    public void SaveCustomer(String id,String name,String address,double salary){
+    public void SaveCustomer(CustomerDto dto){
         System.out.println("Wade Hri");
-        System.out.println(id+""+name+""+address+""+salary);
+        System.out.println(dto.toString());
     }
 }
