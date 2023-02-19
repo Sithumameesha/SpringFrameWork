b119bae40a3eb26009bf2e5c3044aeea57d1dc36
Sithum <sithumameesha76@gmail.com>
2023-02-19 09:15:56 +0530

    Content Updated With Maven

--- maven_test/src/main/java/lk/ijse/spring/WedAppInitializer.java
+++ maven_test/src/main/java/lk/ijse/spring/WedAppInitializer.java
@@ -1,4 +1,28 @@
 package lk.ijse.spring;
 
-public class WedAppInitializer {
+import lk.ijse.spring.config.WedAppConfig;
+import lk.ijse.spring.config.WedRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WedAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{
+                WedRootConfig.class
+        };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{
+                WedAppConfig.class
+        };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{
+                "/"
+        };
+    }
 }
---
+++ maven_test/src/main/java/lk/ijse/spring/config/WedAppConfig.java
@@ -0,0 +1,14 @@
+package lk.ijse.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"lk.ijse.spring.pojo","lk.ijse.spring.controller"})
+public class WedAppConfig {
+    public WedAppConfig(){
+        System.out.println("WedRoot:Instantiated");
+    }
+}
